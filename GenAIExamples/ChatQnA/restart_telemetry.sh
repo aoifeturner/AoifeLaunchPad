4dd515ff12f55ce3f91c9575fe6790d0d9b1fbe0
Yu Wang <yu.wang6@amd.com>
2025-07-09 10:45:21 -0700

    remove dup

--- GenAIExamples/ChatQnA/restart_telemetry.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Restarting Prometheus and telemetry stack..."
-docker compose -f compose.telemetry.yaml restart prometheus
-echo "Prometheus restarted!"
-
-# Optionally, restart all telemetry services:
-# docker compose -f compose.telemetry.yaml restart
